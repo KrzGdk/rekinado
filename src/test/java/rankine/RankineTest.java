3e68556cc7e91e805dd2a946ffc5f7f2a1b2afe6
Krzysiek <krz.gdk@gmail.com>
2014-11-13 19:43:42 +0100

    init

---
+++ src/test/java/rankine/RankineTest.java
@@ -0,0 +1,14 @@
+package rankine;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Krzysiek on 13/11/2014.
+ */
+public class RankineTest {
+    @Test
+    public void test() {
+        assertEquals(true, true);
+    }
+}
