019f072eac2e8621b6bced32fb791bf07896fd1f
Krzysiek <krz.gdk@gmail.com>
2014-11-13 19:28:51 +0100

    init

---
+++ src/main/java/rankine/Rankine.java
@@ -0,0 +1,11 @@
+package rankine;
+
+/**
+ * Created by Krzysiek on 13/11/2014.
+ */
+public class Rankine {
+
+    public static void main(String[] args) {
+        System.out.println("dupa");
+    }
+}
