bf22620bad5916e00a885bd10f4b2498bfa4ff65
Jock69pl <jock69pl@gmail.com>
2014-12-17 00:22:11 +0100

    poprawka wytrzymalosci

--- src/main/java/trees/TreeGUI.java
+++ src/main/java/trees/TreeGUI.java
@@ -222,6 +222,7 @@ public class TreeGUI implements Runnable {
 	 * @return kolor
 	 */
 	private Color getTrunkColor(){
+		if(paintWeakness) return new Color((int) (255*(1-weakness)),0, (int) (255*weakness));
 		if(fallen ) return fallenColor;
 		if(cracked) return crackedColor;
 		return trunkColor;
@@ -233,7 +234,6 @@ public class TreeGUI implements Runnable {
 	 * @return koloe
 	 */
 	public Color getCrownColor(){
-		System.out.printf(weakness+"\n");
 		if(paintWeakness) return new Color((int) (255*(1-weakness)),0, (int) (255*weakness));
 		if(fallen ) return fallenColor;
 		if(cracked) return crackedColor;
